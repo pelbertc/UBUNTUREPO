140f26ae067d16a7c957da9245aaecfd8dee0890
pelbertc <tufumoon@gmail.com>
2019-01-23 11:51:22 -0700

    update

--- C++/CS124/FUNCTIONS/project03.cpp
+++ C++/CS124/FUNCTIONS/project03.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
 * Program:
-*    Project 02, Monthly Budget
+*    Project 03, Monthly Budget
 *    Brother Wagstaff, CS124
 * Author:
 *    Elbert Prescott
@@ -23,47 +23,49 @@ using namespace std;
  ***********************************************************************/
 double getIncome(double monthlyIncome)
 {
-   //prompt
-   cout << "\tYour monthly income: ";
-   //get the value
-   //double monthlyIncome;
-   cin >> monthlyIncome;
-   return monthlyIncome;
+      //prompt
+      cout << "\tYour monthly income: ";
+      //get the value
+      cin >> monthlyIncome;
+      //return value to main
+      return monthlyIncome;
 }
 
 double getBudgetLiving(double budgetLiving)
 {
-   //prompt
-   cout << "\tYour budgeted living expenses: ";
-   //get the value
-   //double budgetLiving;
-   cin >> budgetLiving;
-   return budgetLiving;
+       //prompt
+       cout << "\tYour budgeted living expenses: ";
+       //get the value
+       cin >> budgetLiving;
+       //return value to main
+       return budgetLiving;
 }
 
 
 double getActualLiving(double actualExpenses)
 {
-   //prompt
-   cout << "\tYour actual living expenses: ";
-   //get the  value
-   // double actualExpenses;
-   cin >> actualExpenses;
-   return actualExpenses;
+       //prompt
+       cout << "\tYour actual living expenses: ";
+       //get the  value
+       cin >> actualExpenses;
+       ///return value to main
+       return actualExpenses;
 }
 
 double getActualTax(double actualTax)
 {
 	//prompt
 	cout << "\tYour actual taxes withheld: "; 
-	//double actualTax;
+	//get the value 
 	cin >> actualTax;
+	//return the value to main
 	return actualTax;
 }
 double getActualTithing(double actualTithing)
 {
-	cout << "\tYour actual tithe offerings: ";
-	//double actualTithing;
+        //prompt
+        cout << "\tYour actual tithe offerings: ";
+	//get the value	
 	cin >> actualTithing;
 	return actualTithing;
 }
@@ -75,10 +77,48 @@ double getActualOther(double actualOther)
 	return actualOther;
 }
 
-double display(double income, double budgetLiving, double actualLiving, double actualTax, double actualTithing, double actualOther)
+double display(double income, double actualTax, double actualTithing, double actualLiving, double actualOther, double budgetLiving )
 {
-  //double income = getIncome();
-	  return 0;
+  
+  // return 0;
+  
+  //Line 1
+  cout << "The following is a report on your monthly expenses\n";
+
+  //Line 2
+  cout << "\tItem" << setw(24) << "Budget" << setw(17) << "Actual\n";
+
+  //Line 3
+  cout << "\t=============== " << "===============" << " ===============\n";
+
+  //Line 4
+  cout << "\tIncome" << setw(11) << "$" << setw(11) << income
+       << setw(5) << "$" << setw(11) << income << endl;
+
+  //Line 5
+  cout << "\tTaxes" << setw(12) << "$" << setw(11) << 0.00
+       << setw(5) << "$" << setw(11) << actualTax << endl;
+
+  //Line 6
+  cout << "\tTithing" << setw(10) << "$" << setw(11) << 0.00
+       << setw(5) << "$" << setw(11) << actualTithing << endl;
+
+  //Line 7
+  cout << "\tLiving" << setw(11) << "$" << setw(11) << budgetLiving
+       << setw(5) << "$" << setw(11) << actualLiving << endl;
+
+  //Line 8
+  cout << "\tOther" << setw(12) << "$" << setw(11) << 0.00
+       << setw(5) << "$" << setw(11) << actualOther << endl;
+
+  //Line 9
+  cout << "\t=============== " << "===============" << " ===============\n";
+
+  //Differences between dugdet and actuals
+
+  cout << "\tDifference" << setw(7) << "$" << setw(11) << 0.00
+       << setw(5) << "$" << setw(11) << 0.00 << endl;
+
 }
 
 /**********************************************************************
@@ -102,10 +142,10 @@ int main()
    double otherExpenses;
    
 
-   //Display line 1
+   //Greeting Line
    cout << "This program keeps track of your monthly budget\n";
 
-   //Display line 2
+   //Request information Line
    cout << "Please enter the following:\n";
 
    //Call monthly income function
@@ -120,46 +160,9 @@ int main()
    double actualTithing = getActualTithing(actualTithes);
    	
    double actualOther = getActualOther(otherExpenses);
+
+   display(income,actualTax,actualTithing,actualLiving,actualOther,budgetLiving);
    
-   //Report of the monthly expenses
-
-   //Line 1
-   cout << "The following is a report on your monthly expenses\n";
-
-   //Line 2
-   cout << "\tItem" << setw(24) << "Budget" << setw(17) << "Actual\n";
-
-   //Line 3
-   cout << "\t=============== " << "===============" << " ===============\n";
-
-   //Line 4
-   cout << "\tIncome" << setw(11) << "$" << setw(11) << income
-        << setw(5) << "$" << setw(11) << income << endl;
-
-   //Line 5
-   cout << "\tTaxes" << setw(12) << "$" << setw(11) << 0.00
-        << setw(5) << "$" << setw(11) << actualTaxes << endl;
-
-   //Line 6
-   cout << "\tTithing" << setw(10) << "$" << setw(11) << 0.00
-        << setw(5) << "$" << setw(11) << actualTithes << endl;
-
-   //Line 7
-   cout << "\tLiving" << setw(11) << "$" << setw(11) << budgetExpenses
-        << setw(5) << "$" << setw(11) << actualExpenses << endl;
-
-   //Line 8
-   cout << "\tOther" << setw(12) << "$" << setw(11) << 0.00
-        << setw(5) << "$" << setw(11) << otherExpenses << endl;
-
-   //Line 9
-   cout << "\t=============== " << "===============" << " ===============\n";
-
-   //Differences between dugdet and actuals
-
-   cout << "\tDifference" << setw(7) << "$" << setw(11) << 0.00
-        << setw(5) << "$" << setw(11) << 0.00 << endl;
-
    return 0;
 
 }
