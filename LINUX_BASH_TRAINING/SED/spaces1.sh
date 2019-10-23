5f857cf4628468af24e8910dbd5f8f28cf9e6bfa
pelbertc <tufumoon@gmail.com>
2019-10-23 08:42:03 -0700

    Oct 23

---
+++ LINUX_BASH_TRAINING/SED/spaces1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# ./space.sh [file-path] [-f | --fix] [-h | --help]
+
+function usage() {
+
+	echo "USAGE: $0 [file-path] [-f | --fix] [-h | --help]"
+	exit 1
+}
+
+FIX=0   #0-do not perform fix 1-perform fix
+
+if [ $# -eq 0 ]; then
+   usage
+fi
+
+while [ $# -gt 0 ]
+do
+   case "$1" in
+      -f|--fix)
+         FIX=1
+	 shift
+	 ;;
+
+      -h|--help)
+         usage
+	 shift
+	 ;;
+      
+ 	* )
+	 if [ -f "$1" ]; then 
+	    FILE="$1"
+	    shift
+	   else
+            usage
+            fi
+            ;;
+    esac
+
+done
+
+if [ $FIX -eq 1 ] && [ -f "$FILE" ]; then
+	echo "Fixing spaces and tabs at the beginning and at the end of lines"
+	sed -i 's/[[:blank:]]\+$//' "$FILE"
+	sed -i 's/^[[:blank:]]\+//' "$FILE"
+	echo "DONE"
+fi
---
+++ LINUX_BASH_TRAINING/SED/spaces3.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# ./space.sh [file-path] [-f | --fix] [-h | --help]
+
+function usage() {
+
+	echo "USAGE: $0 [file-path] [-f | --fix] [-h | --help]"
+	exit 1
+}
+
+FIX=0   #0-do not perform fix 1-perform fix
+
+if [ $# -eq 0 ]; then
+   usage
+fi
+
+while [ $# -gt 0 ]
+do
+   case "$1" in
+      -f|--fix)
+         FIX=1
+	 shift
+	 ;;
+
+      -h|--help)
+         usage
+	 shift
+	 ;;
+      
+ 	* )
+	 if [ -f "$1" ]; then 
+	    FILE="$1"
+	    shift
+	   else
+            usage
+            fi
+            ;;
+    esac
+
+done
+
+if [ $FIX -eq 1 ] && [ -f "$FILE" ]; then
+	echo "Fixing spaces and tabs at the beginning and at the end of lines"
+	sed -i 's/[[:blank:]]\+$//' "$FILE"
+	sed -i 's/^[[:blank:]]\+//' "$FILE"
+	echo "DONE"
+fi
+
+#display graphically space errors
+if [ -f "$FILE" ]; then
+   LINES=0
+
+   while IFS= read -r line
+   do
+      let LINES++
+      #if there is no space issue on a line, just print the line
+      echo "$line" | sed -e '/[[:blank:]]\+$/q9' -e '/^[[:blank:]]\+/q7' >/dev/null
+      if [ $? -eq 0 ]; then
+	      printf %4s "$LINES:" >> temp.txt 
+	      echo "$line" >> temp.txt
+	      continue
+      fi
+
+   done < "$FILE"
+   
+   cat temp.txt
+   rm temp.txt
+fi
